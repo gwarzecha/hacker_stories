c98ef2e96572b4151993ddc021cd3adec0030ae6
gwarzecha <gmwarzecha@tutanota.com>
2025-03-03 20:53:33 -0700

    Get rid of boiler plate, add some new content to app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from "react";
+
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 3,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+  {
+    title: "JavaScript",
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    author: "Brendan Eich",
+    num_comments: 1,
+    points: 10,
+    objectID: 2,
+  },
+  {
+    title: "TypeScript",
+    url: "https://www.typescriptlang.org/",
+    author: "Microsoft",
+    num_comments: 2,
+    points: 5,
+    objectID: 3,
+  },
+  {
+    title: "Node.js",
+    url: "https://nodejs.org/",
+    author: "Ryan Dahl",
+    num_comments: 4,
+    points: 5,
+    objectID: 4,
+  },
+  {
+    title: "Python",
+    url: "https://www.python.org/",
+    author: "Guido van Rossum",
+    num_comments: 3,
+    points: 10,
+    objectID: 5,
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+
+      <hr />
+
+      {list.map((item) => {
+        return (
+          <li key={item.objectID}>
+            <a href={item.url} target="_blank">
+              {item.title}
+            </a>{" "}
+            <span>{item.author}</span> <span>{item.num_comments}</span>{" "}
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </div>
+  );
 }
 
-export default App
+export default App;
